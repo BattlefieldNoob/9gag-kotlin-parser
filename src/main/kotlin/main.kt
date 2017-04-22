67203c89c6af744603cc93aa391a1f218a04f4da
Antonio <antonio.ruggiero93@hotmail.it>
2017-04-22 02:19:09 +0200

    Create classi NineGagParser e NineGagPosts

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,27 +1,45 @@
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
 /**
  * Created by antonio on 20/04/17.
  */
 
 fun main(args: Array<String>) {
-    val doc = Jsoup.connect("http://9gag.com").get()
-    val next=doc.getElementsByClass("badge-load-more-post").first().attr("href")
-    var elem=doc.getElementById("container").getElementsByClass("badge-entry-collection").first();
-    for(article in elem.getElementsByTag("article")){
-        val img=article.getElementsByTag("img")
-        if(img==null || img.isEmpty())
-            println("\n NOT A IMAGE  \n")
-        else
-            println(img.first().attr("src"))
+    val parser=NineGagParser()
+    val posts=parser.getPosts(10)
+    parser.getPosts(10,posts.nextPage)
+}
+
+class NineGagParser {
+    val baseUrl = "http://9gag.com"
+    object Pages {
+        val HOT_PAGE = "/hot"
     }
-    println("http://9gag.com"+next.split("%")[0])
-    elem=Jsoup.connect("http://9gag.com"+next.split("%")[0]).get().getElementById("container").getElementsByClass("badge-entry-collection").first();
-    for(article in elem.getElementsByTag("article")){
-        val img=article.getElementsByTag("img")
-        if(img==null || img.isEmpty())
-            println("\n NOT A IMAGE  \n")
+
+    fun getPosts(count: Int, nextPage: String = ""): NineGagPosts {
+        val doc: Document
+
+        if (nextPage.isEmpty())
+            doc = Jsoup.connect(baseUrl).get()
         else
-            println(img.first().attr("src"))
+            doc = Jsoup.connect(baseUrl + nextPage).get()
+
+        val next = doc.getElementsByClass("badge-load-more-post").first().attr("href")
+
+        val list = ArrayList<String>()
+
+        for (article in doc.getElementsByTag("article")) {
+            val img = article.getElementsByTag("img")
+            if (img == null || img.isEmpty())
+                println("\n NOT A IMAGE  \n")
+            else {
+                println(img.first().attr("src"))
+                list.add(img.first().attr("src"))
+            }
+        }
+        return NineGagPosts(list, next.split("%")[0])
     }
 }
+
+data class NineGagPosts(val imagesUrl: List<String>, val nextPage: String)
