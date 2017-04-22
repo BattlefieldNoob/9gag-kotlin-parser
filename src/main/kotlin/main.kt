e11fb85a9fb994c7a6ab2556843ebf2a4bb44841
Antonio <antonio.ruggiero93@hotmail.it>
2017-04-23 01:31:27 +0200

    Very basic server

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,12 @@
+import com.google.gson.Gson
+import org.jetbrains.ktor.application.call
 import org.jsoup.Jsoup
+import org.jetbrains.ktor.host.embeddedServer
+import org.jetbrains.ktor.http.ContentType
+import org.jetbrains.ktor.netty.Netty
+import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.routing.get
+import org.jetbrains.ktor.routing.routing
 import org.jsoup.nodes.Document
 
 /**
@@ -8,7 +16,27 @@ import org.jsoup.nodes.Document
 fun main(args: Array<String>) {
     val parser=NineGagParser()
     val posts=parser.getPosts(10)
-    parser.getPosts(10,posts.nextPage)
+    parser.getPosts(10,posts.paging)
+
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/{section}/{nextPageId}") {
+                val section=call.parameters["section"]
+                val id=call.parameters["nextPageId"]
+                val request=parser.getPosts(10,id);
+                val json=Gson().toJson(request)
+                call.respondText("Hello, world!, ${section} and ${id}   $json", ContentType.Text.Html)
+            }
+
+            get("/{section}"){
+                val section=call.parameters["section"]
+                val request=parser.getPosts(10);
+                val json=Gson().toJson(request)
+                call.respondText("Hello, world!, ${section}  $json", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+    println("Server Start!")
 }
 
 class NineGagParser {
@@ -17,17 +45,17 @@ class NineGagParser {
         val HOT_PAGE = "/hot"
     }
 
-    fun getPosts(count: Int, nextPage: String = ""): NineGagPosts {
+    fun getPosts(count: Int, nextPage: String? = ""): GagRequest {
         val doc: Document
 
-        if (nextPage.isEmpty())
+        if (nextPage==null || nextPage.isEmpty())
             doc = Jsoup.connect(baseUrl).get()
         else
             doc = Jsoup.connect(baseUrl + nextPage).get()
 
         val next = doc.getElementsByClass("badge-load-more-post").first().attr("href")
 
-        val list = ArrayList<String>()
+        val list = ArrayList<Gag>()
 
         for (article in doc.getElementsByTag("article")) {
             val img = article.getElementsByTag("img")
@@ -35,11 +63,14 @@ class NineGagParser {
                 println("\n NOT A IMAGE  \n")
             else {
                 println(img.first().attr("src"))
-                list.add(img.first().attr("src"))
+                val link=img.first().attr("src")
+                list.add(Gag("ID","HELLO", hashMapOf("small" to link,"cover" to link,"normal" to link,"large" to link)))
             }
         }
-        return NineGagPosts(list, next.split("%")[0])
+        return GagRequest(200,"OK",list, next.split("%")[0])
     }
 }
 
-data class NineGagPosts(val imagesUrl: List<String>, val nextPage: String)
+data class GagRequest(val status:Int,val message:String, val data: List<Gag>, val paging: String)
+
+data class Gag(val id:String,val caption:String,val images:HashMap<String,String>)
