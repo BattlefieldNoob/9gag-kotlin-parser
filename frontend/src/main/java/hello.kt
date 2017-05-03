bdf84dc0c07bb092585254fb580e49d97f85b74b
macmini.dev <antonio.ruggiero93@hotmail.it>
2017-05-03 16:26:21 +0200

    Finally all works!!

--- frontend/src/main/java/hello.kt
+++ frontend/src/main/java/hello.kt
@@ -1,4 +1,5 @@
 
+import App.React9gagFeed
 import kotlinx.html.div
 import kotlinx.html.h1
 import kotlinx.html.img
@@ -22,89 +23,9 @@ import kotlin.js.json
 
 
 fun main(args: Array<String>) {
-    console.log("HELLO!")
     ReactDOM.render(document.getElementById("react-app")){
         div {
             React9gagFeed {}
         }
     }
-}
-
-class HelloWorld : ReactDOMComponent<ReactComponentNoProps,ReactComponentNoState>(){
-    companion object : ReactComponentSpec<HelloWorld, ReactComponentNoProps, ReactComponentNoState>
-
-    init {
-        state= ReactComponentNoState()
-    }
-
-    override fun componentDidMount() {
-        super.componentDidMount()
-    }
-
-    override fun ReactDOMBuilder.render() {
-        div {
-            React9gagFeed()
-        }
-    }
-}
-
-class React9gagPhoto : ReactDOMComponent<React9gagPhoto.Props,ReactComponentNoState>(){
-    companion object : ReactComponentSpec<React9gagPhoto, Props, ReactComponentNoState>
-
-    class Props(var url:String):RProps()
-
-    init {
-        console.log("Stated photo")
-    }
-
-    override fun ReactDOMBuilder.render() {
-        div{
-            img(src = props.url)
-        }
-    }
-}
-
-class React9gagFeed : ReactDOMComponent<ReactComponentNoProps,BoxedState<String>>(){
-    companion object : ReactComponentSpec<React9gagFeed, ReactComponentNoProps, BoxedState<String>>
-
-    init {
-        console.log("Stated Feed")
-        state = BoxedState("")
-        console.log("Status:$state")
-        requestAndParseResult("GET","/photo/hot",null,parse = ::parse9gagJson).then({ response ->
-            {
-                console.log("Response:$response")
-            }
-        })
-
-    }
-
-    override fun ReactDOMBuilder.render() {
-        div{
-            if(state.state.isNotEmpty()){
-                console
-                React9gagPhoto{
-                    url=state.state
-                }
-            }
-        }
-    }
-
-}
-
-fun <T> requestAndParseResult(method: String, url: String, body: dynamic, parse: (dynamic) -> T): Promise<T> {
-    console.log("REQUEST")
-    return Promise { resolve, reject ->
-        window.fetch(url, object: RequestInit {
-            override var method: String? = method
-            override var body: dynamic = body
-            override var headers: dynamic = json("Accept" to "application/json")
-        }).then({ response ->
-                response.json().then({ resolve(parse(it)) }, reject).catch(reject)
-        }, reject).catch(reject)
-    }
-}
-
-private fun parse9gagJson(json:dynamic):String {
-    return json.images.small
 }
