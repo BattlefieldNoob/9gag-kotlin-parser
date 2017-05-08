13649c2cacc97c5eee41fb2366bcc11d8d4b8806
macmini.dev <antonio.ruggiero93@hotmail.it>
2017-05-08 14:40:28 +0200

    Netty To Jetty, Configured with conf file

--- backend/src/main/java/main.kt
+++ backend/src/main/java/main.kt
@@ -1,8 +1,11 @@
 import com.google.gson.Gson
+import org.jetbrains.ktor.application.Application
 import org.jetbrains.ktor.application.call
+import org.jetbrains.ktor.application.install
 import org.jetbrains.ktor.content.files
 import org.jetbrains.ktor.content.serveFileSystem
 import org.jetbrains.ktor.content.static
+import org.jetbrains.ktor.features.DefaultHeaders
 import org.jetbrains.ktor.host.ApplicationHostEnvironment
 import org.jetbrains.ktor.host.ApplicationHostEnvironmentBuilder
 import org.jsoup.Jsoup
@@ -10,8 +13,7 @@ import org.jetbrains.ktor.host.embeddedServer
 import org.jetbrains.ktor.html.respondHtmlTemplate
 import org.jetbrains.ktor.http.ContentType
 import org.jetbrains.ktor.http.HttpStatusCode
-import org.jetbrains.ktor.netty.Netty
-import org.jetbrains.ktor.netty.NettyApplicationHost
+import org.jetbrains.ktor.jetty.JettyApplicationHost
 import org.jetbrains.ktor.response.respondText
 import org.jetbrains.ktor.routing.get
 import org.jetbrains.ktor.routing.routing
@@ -22,40 +24,39 @@ import java.io.File
  * Created by antonio on 20/04/17.
  */
 
-fun main(args: Array<String>) {
+fun Application.main(){
     val parser=NineGagParser()
-    println("Server Start!")
     val file = File("public")
     println(file.absolutePath)
-    embeddedServer(Netty, 8080) {
-        routing {
-            get("/next/{section}/{nextPageId}") {
-                val section=call.parameters["section"]
-                val id=call.parameters["nextPageId"]
-                val request=parser.getPosts(10,id);
-                val json=Gson().toJson(request)
-                call.respondText(json, ContentType.Application.Json)
-            }
 
-            get("/"){
-                call.respondHtmlTemplate(IndexPage(), HttpStatusCode.OK){
+    install(DefaultHeaders)
+    routing {
+        get("/next/{section}/{nextPageId}") {
+            val section=call.parameters["section"]
+            val id=call.parameters["nextPageId"]
+            val request=parser.getPosts(10,id);
+            val json=Gson().toJson(request)
+            call.respondText(json, ContentType.Application.Json)
+        }
 
-                }
-            }
+        get("/"){
+            call.respondHtmlTemplate(IndexPage(), HttpStatusCode.OK){
 
-            static {
-                files(file)
-            }
-
-            get("/photo/{section}"){
-                val section=call.parameters["section"]
-                val request=parser.getPosts(10);
-                println(request)
-                val json=Gson().toJson(request)
-                call.respondText(json, ContentType.Application.Json)
             }
         }
-    }.start(wait = true)
+
+        static {
+            files(file)
+        }
+
+        get("/photo/{section}"){
+            val section=call.parameters["section"]
+            val request=parser.getPosts(10);
+            println(request)
+            val json=Gson().toJson(request)
+            call.respondText(json, ContentType.Application.Json)
+        }
+    }
 }
 
 class NineGagParser {
